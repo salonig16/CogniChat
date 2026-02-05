2a6160b31ef93f1bff89e388e2b625c46daddad6
salonig16 <165128934+salonig16@users.noreply.github.com>
2026-02-05 19:50:15 +0530

    Update chat.js

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -4,7 +4,6 @@ import getOpenAIAPIResponse from "../utils/openai.js";
 
 const router = express.Router();
 
-//test
 router.post("/test", async(req,res) =>{
     try{
         const thread = new Thread({
