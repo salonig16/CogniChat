e95f2775227d9f3d7e47ae1be3940d5a132b8b84
salonig16 <165128934+salonig16@users.noreply.github.com>
2026-03-04 21:15:29 +0530

    Update Chat.jsx

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -3,9 +3,7 @@ import React, {useContext, useState, useEffect} from "react";
 import { MyContext } from "./MyContext";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
-import "highlight.js/styles/github-dark.css"; //for styling our syntax on website on chatWindow
-
-//react-markdown //rehype-highlight
+import "highlight.js/styles/github-dark.css"; 
 
 function Chat(){
     const {newChat, prevChats, reply} = useContext(MyContext);
@@ -13,14 +11,13 @@ function Chat(){
 
     useEffect (()=>{
        if(reply === null){
-          setLatestReply( null); //prev chat load
+          setLatestReply( null); 
           return;
        }
 
-        //latestReply separate => typing effect create
         if(!prevChats?.length) return;
 
-        const content = reply.split(" "); //individual word is stored
+        const content = reply.split(" "); 
         let idx =0;
         const interval = setInterval(() =>{
            setLatestReply(content.slice(0, idx+1).join(" "));
@@ -37,7 +34,7 @@ function Chat(){
          {newChat && <h1>Start a New Chat!</h1>}
          <div className="chats">
             {
-                prevChats?.slice(0, -1).map((chat, idx)=> //latest reply show nhi hoga
+                prevChats?.slice(0, -1).map((chat, idx)=>
                   <div className={chat.role === "user" ? "userDiv" : "gptDiv"} key={idx}> 
                        {
                         chat.role === "user" ? 
@@ -48,7 +45,6 @@ function Chat(){
                 )
             }
 
-            {/* Ternary operation for writing conditional statement */}
             {
                prevChats.length > 0 && (
                   <>
@@ -66,34 +62,7 @@ function Chat(){
                   </>
                )
             }
-   
-             
 
-
-             {/* without using ternary operator */}
-
-             {/* {
-                prevChats.length > 0 && latestReply !== null && 
-                <div className="gptDiv" key={"typing"}>
-                   <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{latestReply}</ReactMarkdown>
-                </div>
-             }
-
-             {
-                prevChats.length > 0 && latestReply === null && 
-                <div className="gptDiv" key={"non-typing"}>
-                   <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{prevChats[prevChats.length-1].content}</ReactMarkdown>
-                </div>
-             } */}
-
-
-            {/* static data
-            <div className="userDiv">
-               <p className="userMessage"></p>
-            </div>
-            <div className="gptDiv">
-               <p className="gptMessage"></p>
-            </div> */}
          </div>
         </>
     )
