1d8573b7446f9761cd7c70c4253f53ac358072a9
salonig16 <165128934+salonig16@users.noreply.github.com>
2026-03-04 21:00:48 +0530

    Update openai.js

--- Backend/utils/openai.js
+++ Backend/utils/openai.js
@@ -1,14 +1,14 @@
 import "dotenv/config";
 
 const getOpenAIAPIResponse = async(message) => {
-   const options = { //object for fetch
+   const options = { 
     method: "POST", 
     headers:{
       "Content-Type":"application/json",
       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
     },
     body: JSON.stringify({
-      model:"gpt-4o-mini", //required parameters
+      model:"gpt-4o-mini", 
       messages: [{
         role:"user",
         content: message
@@ -19,8 +19,7 @@ const getOpenAIAPIResponse = async(message) => {
   try {
     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
     const data = await response.json();
-    //console.log(data);
-    return data.choices[0].message.content; //reply
+    return data.choices[0].message.content; 
   } catch(err){
      console.log(err);
   }
