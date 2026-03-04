a4eba7d7cdb342c2728fdd491bf4c2d2b7cbfbb8
salonig16 <165128934+salonig16@users.noreply.github.com>
2026-03-04 21:01:58 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,3 @@
-//way1
-// import OpenAI from 'openai';
-// import 'dotenv/config';
-
-// const client = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY, // This is the default and can be omitted
-// });
-
-// const response = await client.responses.create({
-//   model: 'gpt-4o-mini',
-//   input: 'Joke related to Computer Science',
-// });
-
-// console.log(response.output_text);
-
-//way2
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
@@ -40,34 +24,3 @@ const connectDB = async() =>{
     console.log("Failed to connect to DB",err);
    }
 }
-
-//utility based core logic of our project
-// app.post("/test", async (req, res) =>{ //api endpoint / frontend - query ayegi - will send this query to openai - the response to it will again be send on frontend
-//   const options = { //object for fetch
-//     method: "POST", 
-//     headers:{
-//       "Content-Type":"application/json",
-//       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-//     },
-//     body: JSON.stringify({
-//       model:"gpt-4o-mini", //required parameters
-//       messages: [{
-//         role:"user",
-//         content: req.body.message
-//       }]
-//     })
-//   };
-  
-//   try {
-//     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-//     const data = await response.json();
-//     //console.log(data);
-//     res.send(data.choices[0].message.content);
-//   } catch(err){
-//      console.log(err);
-//   }
-// });
-
-
-
-
